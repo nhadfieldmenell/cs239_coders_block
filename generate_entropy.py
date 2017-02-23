1292e0d278f3083d3c61170318a048f500971e75
Noah Hadfield-Menell <nhadfieldmenell@gmail.com>
2017-02-22 19:48:34 -0800

    entropy calc beginnings

--- generate_entropy.py
+++ generate_entropy.py
@@ -3,6 +3,7 @@
     Store the dict in entropies.pkl.
 """
 
+import csv
 from collections import defaultdict
 
 class EntropyCalculator:
@@ -13,12 +14,39 @@ class EntropyCalculator:
         self.answers_fn = '../pythonquestions/Answers.csv'
         self.tags_fn = '../pythonquestions/Tags.csv'
 
+    
+    def process_csv(self, questions=True):
+        """Count the occurrences of terms in each document of a question/answer csv file.
+
+        Args:
+            questions: True if processing Questions.csv. False if processing Answers.csv.
+        """
+        in_fn = self.questions_fn if questions else self.answers_fn
+        with open(in_fn, 'rb') as infile:
+            reader = csv.reader(infile)
+            count = 0
+            for line in reader:
+                count += 1
+                if count == 1:
+                    continue
+                if count > 10:
+                    break
+                if questions:
+                    question_id, _, _, _, _, body = line
+                else:
+                    _, _, _, question_id, _, body = line
+                print question_id
+
+
+
     def count_terms(self):
         """Iterate through all discussions (questions & answers) to count the occurrences of terms.
         
         Update term2count and term2discussion2count.
         """
-        pass
+        self.process_csv()
+        print ''
+        self.process_csv(False)
 
 
 
