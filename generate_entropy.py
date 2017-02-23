a5f5bc48c4e3fdf798061934f0a40dab8ef27123
Noah Hadfield-Menell <nhadfieldmenell@gmail.com>
2017-02-22 19:36:33 -0800

    entropy calc beginnings

--- generate_entropy.py
+++ generate_entropy.py
@@ -2,3 +2,32 @@
     Iterate through all the questions and answers to generate a dict mapping term to its entropy value.
     Store the dict in entropies.pkl.
 """
+
+from collections import defaultdict
+
+class EntropyCalculator:
+    def __init__(self):
+        self.term2count = defaultdict(int)
+        self.term2discussion2count = {}
+        self.questions_fn = '../pythonquestions/Questions.csv'
+        self.answers_fn = '../pythonquestions/Answers.csv'
+        self.tags_fn = '../pythonquestions/Tags.csv'
+
+    def count_terms(self):
+        """Iterate through all discussions (questions & answers) to count the occurrences of terms.
+        
+        Update term2count and term2discussion2count.
+        """
+        pass
+
+
+
+def main():
+    e = EntropyCalculator()
+    e.count_terms()
+
+
+if __name__ == '__main__':
+    main()
+
+
