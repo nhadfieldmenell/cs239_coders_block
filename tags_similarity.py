37db44dae2a7490c193c4b968fc83ad0ca4d02c2
Orpaz Goldstein <orpgol@gmail.com>
2017-02-25 19:39:03 -0800

    Aggregating tags
    Aggregating tags into a set to test intersection of tags with the query

--- tags_similarity.py
+++ tags_similarity.py
@@ -16,8 +16,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # In[6]:
 
+'''
+Iterate through the entire tags database, preprocess the content of the tags
+and creare an Id -> set of tags dataframe to be used for scoring the tag similarity of the query.
+'''
 # Load data and use correct encoding 
-qdf = pd.read_csv('pythonquestions/Questions.csv', encoding='iso-8859-1')
+#qdf = pd.read_csv('pythonquestions/Questions.csv', encoding='iso-8859-1')
 tdf = pd.read_csv('pythonquestions/Tags.csv', encoding='iso-8859-1')
 # Using nltk data for stop words etc'
 nltk.data.path.append('/Users/orpaz/Developer/nltk_data')
@@ -32,14 +36,17 @@ def stem_tags(data):
             data.set_value(i, "Tag", st)
 
 
-# In[40]:
-
-if __name__ == '__main__':
-    stem_tags(tdf)
-    #df = qdf.join(tdf.set_index('Id'), on='Id')
-
-
 # In[ ]:
 
+def aggregate_tags(data):
+    data = data.groupby('Id').agg(lambda x: set(x))
 
+
+# In[40]:
+
+# The result of these step can be tested for the intersection with the tags from the query
+if __name__ == '__main__':
+    stem_tags(tdf)
+    aggregate_tags(tdf)
+    #df = qdf.join(tdf.set_index('Id'), on='Id')
 
