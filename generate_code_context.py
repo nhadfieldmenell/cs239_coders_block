0fdfe5172d0d85931cf59a2b2a8e745c6345fe83
Noah Hadfield-Menell <nhadfieldmenell@gmail.com>
2017-03-01 14:28:53 -0800

    levenshtein skeleton

--- generate_code_context.py
+++ generate_code_context.py
@@ -7,6 +7,7 @@
 import ast
 from collections import deque
 import pickle
+from Levenshtein import distance
 
 class FuncCallVisitor(ast.NodeVisitor):
     '''
@@ -64,6 +65,36 @@ def get_func_calls(tree):
         'func_calls': func_calls,
         'imports': imports
         }
+
+def levenshtein(a, b):
+    '''Compute the Levenshtein distance between two strings.
+
+    Args:
+        a: A string.
+        b: A string.
+
+    Returns:
+        The int levenshtein distance between the two.
+    '''
+    return distance(a, b)
+
+def prune_typos(word_frequency, term2entropy):
+    '''Prune out words whose Levenshtein distance == 1 according to alg in paper.
+
+    Args:
+        word_frequency: A list of tuples (x, y) such that:
+            x: A word.
+            y: The word's frequency in the code context.
+        term2entropy: A dict mapping a strin term to its entropy value.
+
+    Returns:
+        The same word_frequency list with typos pruned and frequencies added to their counterparts.
+
+    Example:
+        input: [('what', .5), ('whit', .1), ('blah', .4)
+        output: [('what', .6), ('blah', .4)]
+    '''
+    pass
 
 if __name__ == '__main__':
     import argparse
