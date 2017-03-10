1f7004c3d91589b76c8c68b7145abe58f3878305
Orpaz Goldstein <orpgol@gmail.com>
2017-03-10 01:08:33 -0800

    functions for scoring script added

--- tags_similarity.py
+++ tags_similarity.py
@@ -14,15 +14,17 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-# In[2]:
+# In[3]:
 
 '''
 Iterate through the entire tags database, preprocess the content of the tags
 and creare an Id -> set of tags dataframe to be used for scoring the tag similarity of the query.
 '''
 
+df = pd.read_csv('pythonquestions/processed_discussions.csv', encoding='iso-8859-1')
 
-# In[3]:
+
+# In[78]:
 
 def stem_tags(data):
     for i, row in data.iterrows():
@@ -31,13 +33,34 @@ def stem_tags(data):
             data.set_value(i, "Tag", st)
 
 
-# In[17]:
+# In[79]:
 
 def aggregate_tags(data):
-    return (data.groupby('Id').agg(lambda x: set(x)))
+    return (data.groupby('Id').agg(lambda x: set(x))).reset_index()
 
 
-# In[ ]:
+# In[56]:
+
+def tags_score(d):
+    lst = list()
+    for i, row in df.iterrows():
+        try:
+            if row['Tag']:
+                #print(row['Tag'], i)
+                if 'nan,' in row['Tag'] or 'nan}' in row['Tag']:
+                    s = eval(row['Tag'].replace('nan', "''")) & d['imports']
+                else:
+                    s = eval(row['Tag']) & d['imports']
+                res = len(s) / len(row['Tag'])
+                lst.append(res)
+            else:
+                lst.append(0)
+        except:
+            print(row['Tag'], i)
+    return lst
+
+
+# In[74]:
 
 # The result of these step can be tested for the intersection with the tags from the query
 if __name__ == '__main__':
@@ -48,10 +71,5 @@ if __name__ == '__main__':
     nltk.data.path.append('/Users/orpaz/Developer/nltk_data')
     stem_tags(tdf)
     tdf = aggregate_tags(tdf)
-    df = qdf.join(tdf.set_index('Id'), on='Id')
-
-
-# In[ ]:
-
-
+    #df = qdf.join(tdf.set_index('Id'), on='Id')
 
--- textual_similarity.py
+++ textual_similarity.py
@@ -22,6 +22,10 @@ Iterate through the entire question database, preprocess the content of the 'Bod
 and creare a Tf/Idf matrix to be used for scoring the query from the client.
 '''
 
+df = pd.read_csv('pythonquestions/Discussions.csv', encoding='iso-8859-1')
+nltk.data.path.append('/Users/orpaz/Developer/nltk_data')
+disc = pickle.load(open('tex_sim_pkls/questions_tfidf.pkl', 'rb'))
+
 
 # In[12]:
 
@@ -98,7 +102,7 @@ def vectorizer(data, save = False, query = False):
     return X
 
 
-# In[43]:
+# In[2]:
 
 def cosine_sim(query, base):
     cosine_similarities = linear_kernel(query, base).flatten()
@@ -106,72 +110,24 @@ def cosine_sim(query, base):
     related_docs_indices = cosine_similarities.argsort()[:-7:-1]
     print("Indices of best docs: " + str(related_docs_indices))
     print("Best docs cosine score: " + str(cosine_similarities[related_docs_indices]))
-    return related_docs_indices
+    return cosine_similarities
 
 
 # In[87]:
 
-def tf_score(d, df):
-    nltk.data.path.append('/Users/orpaz/Developer/nltk_data')
-    disc = pickle.load(open('tex_sim_pkls/questions_tfidf.pkl', 'rb'))
-    
+def tf_score(d):
     d['code'] = stem_data(d, query = True)
     d['code'] = remove_stop_words(d, query = True)
     X = vectorizer(d, query = True)
-    related_docs = cosine_sim(X,disc)
-    print('\n Example result: \n' + df.iloc[related_docs[2]]['Body'])
+    return cosine_sim(X,disc)
 
 
 # In[48]:
 
-if __name__ == '__main__':
-    # Load data and use correct encoding 
-    df = pd.read_csv('pythonquestions/Discussions.csv', encoding='iso-8859-1')
+if __name__ == '__main__': 
     # Using nltk data for stop words etc'
-    nltk.data.path.append('/Users/orpaz/Developer/nltk_data')
     stem_data(df)
     remove_stop_words(df)
     vectorizer(df)
-    df.to_csv('pythonquestions/processed_discussions.csv', encoding='iso-8859-1')
-
-
-# In[88]:
-
-def test_func(df):
-
-    ## An example query containing quicksort algorithm
-    d = {'imports' : {}, 'methods' : ['quickSort', 'quickSortHelper', 'partition'], 
-        'code' : '''function quicksort(array)
-        if length(array) > 1
-            pivot := select any element of array
-            left := first index of array
-            right := last index of array
-            while left ≤ right
-                while array[left] < pivot
-                    left := left + 1
-                while array[right] > pivot
-                    right := right - 1
-                if left ≤ right
-                    swap array[left] with array[right]
-                    left := left + 1
-                    right := right - 1
-            quicksort(array from first index to right)
-            quicksort(array from left to last index)'''}
-
-    tf_score(d, df)
-
-
-# In[82]:
-
-df = pd.read_csv('pythonquestions/Discussions.csv', encoding='iso-8859-1')
-
-
-# In[89]:
-
-test_func(df)
-
-
-# In[ ]:
-
-
+    df.to_csv('pythonquestions/processed_discussions.csv', encoding='iso-8859-1', index=False)
 
