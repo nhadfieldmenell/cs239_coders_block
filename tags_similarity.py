149019a3c7ed3a66193cf0b3b243f9ee7c9c915b
Orpaz Goldstein <orpgol@gmail.com>
2017-03-21 16:29:00 -0700

    improved speed tags scoring

--- tags_similarity.py
+++ tags_similarity.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[11]:
 
 import pandas as pd
 import numpy as np
@@ -14,7 +14,7 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-# In[3]:
+# In[12]:
 
 '''
 Iterate through the entire tags database, preprocess the content of the tags
@@ -24,7 +24,7 @@ and creare an Id -> set of tags dataframe to be used for scoring the tag similar
 df = pd.read_csv('pythonquestions/processed_discussions.csv', encoding='iso-8859-1')
 
 
-# In[78]:
+# In[6]:
 
 def stem_tags(data):
     for i, row in data.iterrows():
@@ -33,42 +33,49 @@ def stem_tags(data):
             data.set_value(i, "Tag", st)
 
 
-# In[79]:
+# In[7]:
 
 def aggregate_tags(data):
     return (data.groupby('Id').agg(lambda x: set(x))).reset_index()
 
 
-# In[56]:
+# In[24]:
 
 def tags_score(d):
     lst = list()
-    for i, row in df.iterrows():
+    for i, row in df['Tag'].iteritems():
         try:
-            if row['Tag']:
+            if row:
                 #print(row['Tag'], i)
-                if 'nan,' in row['Tag'] or 'nan}' in row['Tag']:
-                    s = eval(row['Tag'].replace('nan', "''")) & d['imports']
+                if 'nan,' in row or 'nan}' in row:
+                    s = eval(row.replace('nan', "''")) & d['imports']
                 else:
-                    s = eval(row['Tag']) & d['imports']
-                res = len(s) / len(row['Tag'])
+                    s = eval(row) & d['imports']
+                res = len(s) / len(row)
                 lst.append(res)
             else:
                 lst.append(0)
         except:
-            print(row['Tag'], i)
+            print(row, i)
     return lst
 
 
-# In[74]:
+# In[6]:
 
 # The result of these step can be tested for the intersection with the tags from the query
 if __name__ == '__main__':
-    # Load data and use correct encoding
+    # Load data and use correct encoding 
     qdf = pd.read_csv('pythonquestions/processed_discussions.csv', encoding='iso-8859-1')
     tdf = pd.read_csv('pythonquestions/Tags.csv', encoding='iso-8859-1')
     # Using nltk data for stop words etc'
     nltk.data.path.append('/Users/orpaz/Developer/nltk_data')
     stem_tags(tdf)
     tdf = aggregate_tags(tdf)
-    #df = qdf.join(tdf.set_index('Id'), on='Id')
+    df = qdf.join(tdf.set_index('Id'), on='Id')
+    df.to_csv('pythonquestions/processed_discussions.csv', encoding='iso-8859-1', index=False)
+
+
+# In[ ]:
+
+
+
